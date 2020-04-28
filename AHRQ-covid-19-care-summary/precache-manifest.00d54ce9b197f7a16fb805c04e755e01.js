a4cfc36e3ad2eaedf13f41b70257657b60f7eb09
“Dave <dcarlson@clinicalcloud.solutions>
2020-04-28 16:44:15 -0600

    Initial commit

---
+++ AHRQ-covid-19-care-summary/precache-manifest.00d54ce9b197f7a16fb805c04e755e01.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66f9f80ab9a0b9dc30798bdba2813e1f",
+    "url": "/AHRQ-covid-19-care-summary/index.html"
+  },
+  {
+    "revision": "a1dc3bea0fb5910fe33c",
+    "url": "/AHRQ-covid-19-care-summary/static/css/main.d8b70692.chunk.css"
+  },
+  {
+    "revision": "196861bbe3ba7ed0d556",
+    "url": "/AHRQ-covid-19-care-summary/static/js/0.2d9d898d.chunk.js"
+  },
+  {
+    "revision": "5cb23d752498288fc337",
+    "url": "/AHRQ-covid-19-care-summary/static/js/5.45995c28.chunk.js"
+  },
+  {
+    "revision": "1b3a056358d5e7ee326b4bba0750a7d3",
+    "url": "/AHRQ-covid-19-care-summary/static/js/5.45995c28.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c82ae2faa8bc0d963abe",
+    "url": "/AHRQ-covid-19-care-summary/static/js/launch.9d21efbd.chunk.js"
+  },
+  {
+    "revision": "a1dc3bea0fb5910fe33c",
+    "url": "/AHRQ-covid-19-care-summary/static/js/main.1969aa26.chunk.js"
+  },
+  {
+    "revision": "3b59e21409c3e69f904a",
+    "url": "/AHRQ-covid-19-care-summary/static/js/runtime-launch.168bb4c4.js"
+  },
+  {
+    "revision": "c74a26e4ee4233279a86",
+    "url": "/AHRQ-covid-19-care-summary/static/js/runtime-main.11fd079a.js"
+  }
+]);
