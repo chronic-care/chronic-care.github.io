95a91f4786d6dbcd6467801445f535afab5a7b7f
Dave Carlson <dcarlson@ClinicalCloud.solutions>
2021-03-21 12:23:06 -0600

    MPC patient app

---
+++ mpc/precache-manifest.931951f57f2cbe87fc2a12c3af893650.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da6cfe32702ac1603669ac8da97b5348",
+    "url": "/mpc/index.html"
+  },
+  {
+    "revision": "6d6c399cde8e8f5bf776",
+    "url": "/mpc/static/css/5.7459dfad.chunk.css"
+  },
+  {
+    "revision": "b3ebb858b6fed1c7a2dc",
+    "url": "/mpc/static/css/main.b25e511b.chunk.css"
+  },
+  {
+    "revision": "ed98ab17d57361a89e1d",
+    "url": "/mpc/static/js/0.224b9fbd.chunk.js"
+  },
+  {
+    "revision": "6d6c399cde8e8f5bf776",
+    "url": "/mpc/static/js/5.20527310.chunk.js"
+  },
+  {
+    "revision": "65ed62bf58d12738d2495a6d015eb77a",
+    "url": "/mpc/static/js/5.20527310.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecc04bde96f9b3ffa946",
+    "url": "/mpc/static/js/launch.f675bc47.chunk.js"
+  },
+  {
+    "revision": "b3ebb858b6fed1c7a2dc",
+    "url": "/mpc/static/js/main.92175b56.chunk.js"
+  },
+  {
+    "revision": "9dcd5ab89519b3dacb1d",
+    "url": "/mpc/static/js/runtime-launch.66c66000.js"
+  },
+  {
+    "revision": "85b379d71f3134075fb6",
+    "url": "/mpc/static/js/runtime-main.61401684.js"
+  }
+]);
